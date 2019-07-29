fd5ef3b1804c936ac3c2413166ebebf7207d4597
Jake Scott <bemybanker@gmail.com>
2019-07-29 14:49:21 -0500

    Navbar displaying

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,1 +1,31 @@
-//initial file structure 
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Problems
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/newIssue">
+              New Issue
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/mappie">
+              Mappie
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
---
+++ src/problems.js
@@ -0,0 +1,18 @@
+import React, { Component} from "react"
+import NavBar from "./components/nav/navbar"
+// import ApplicationViews from "./ApplicationViews"
+import "./problems.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
+
+class Problems extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />
+                {/* <ApplicationViews /> */}
+            </React.Fragment>
+        )
+    }
+}
+
+export default Problems
